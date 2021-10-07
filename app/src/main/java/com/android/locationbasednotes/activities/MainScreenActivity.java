217c11e659be348ea1cb820fd63013a8eb832299
TalZaidman <talzaidman5@gmail.com>
2021-10-07 18:39:15 +0300

    update

--- app/src/main/java/com/android/locationbasednotes/activities/MainScreenActivity.java
+++ app/src/main/java/com/android/locationbasednotes/activities/MainScreenActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -25,7 +27,7 @@ import com.google.gson.Gson;
 
 public class MainScreenActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
-    private Button activity_main_BTN_logout;
+    private Button activity_main_BTN_signout;
     private FloatingActionButton activity_main_BTN_createNewNote;
     private MySheredP msp;
     private BottomNavigationView activity_main_NGV_navigationMenu;
@@ -52,15 +54,30 @@ public class MainScreenActivity extends AppCompatActivity {
             }
         });
 
-        activity_main_BTN_logout.setOnClickListener(new View.OnClickListener() {
+        activity_main_BTN_signout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mAuth.signOut();
-                finish();
-                startNewActivity(MainActivity.class);
+                openDialogSignOut(view);
             }
         });
 
+    }
+
+    private void openDialogSignOut(View view) {
+        new AlertDialog.Builder(view.getContext())
+                .setTitle(view.getContext().getString(R.string.signup))
+                .setMessage(view.getContext().getString(R.string.are_you_sure_signup))
+
+                .setPositiveButton(view.getContext().getString(R.string.yes), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        mAuth.signOut();
+                        finish();
+                        startNewActivity(MainActivity.class);
+                    }
+                })
+                .setNegativeButton(view.getContext().getString(R.string.no), null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 
     private void startNewActivity(Class<?> newActivity) {
@@ -90,7 +107,7 @@ public class MainScreenActivity extends AppCompatActivity {
 
     private void findViews() {
         activity_main_BTN_createNewNote = findViewById(R.id.activity_main_BTN_createNewNote);
-        activity_main_BTN_logout = findViewById(R.id.activity_main_BTN_logout);
+        activity_main_BTN_signout = findViewById(R.id.activity_main_BTN_logout);
         activity_main_NGV_navigationMenu = findViewById(R.id.activity_main_NGV_navigationMenu);
         activity_main_TXT_noNotes = findViewById(R.id.activity_main_TXT_noNotes);
     }
